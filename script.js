5a60bc95bc5b091bc0d95f7b19bce6ee549969ba
MrAxeman777 <arsharma892@gmail.com>
2026-07-29 09:11:06 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const birthday = new Date("July 28, 2027 00:00:00").getTime();
+
+setInterval(function () {
+
+    const now = new Date().getTime();
+
+    const distance = birthday - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML =
+        `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
+
+}, 1000);
